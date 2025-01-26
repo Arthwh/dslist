85107d320d8c17ebaf79112f1cc43a36f98d12f8
Arthur Gabriel Fabrim Alonso <arthur.fabrin028@gmail.com>
2025-01-26 17:51:20 -0300

    GET games by list

--- src/main/java/com/arthwh/dslist/dto/GameMinDTO.java
+++ src/main/java/com/arthwh/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.arthwh.dslist.dto;
 
 import com.arthwh.dslist.entities.Game;
+import com.arthwh.dslist.projection.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -15,6 +16,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
